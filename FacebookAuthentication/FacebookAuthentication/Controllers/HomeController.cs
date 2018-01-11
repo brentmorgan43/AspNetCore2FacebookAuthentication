ee66527c811d5fd7400b347e7da5509cfff4a0a9
brentmorgan43 <35343056+brentmorgan43@users.noreply.github.com>
2018-01-11 13:36:14 -0600

    Add files via upload

---
+++ FacebookAuthentication/FacebookAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacebookAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FacebookLogin()
+        {
+            // Request a redirect to the external login provider.
+            var redirectUrl = "./Home";
+            var properties = new AuthenticationProperties();
+            properties.RedirectUri = redirectUrl;
+            properties.Items.Add("LoginProvider", "Facebook");
+            var cr = new ChallengeResult("Facebook", properties);
+            return cr;
+        }
+
+    }
+}
